d5e958b15e07c1966e0d05903d4ac0bd3975eb5c
DK\IRO <iro@sitecore.net>
2018-03-21 18:17:56 +0200

    initial to fix General Link field

---
+++ src/Sitecore.Support.211477/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.211477")]
+[assembly: AssemblyProduct("Sitecore.Support.211477")]
+[assembly: ComVisible(false)]
