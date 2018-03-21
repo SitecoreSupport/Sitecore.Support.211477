9b5e2ef3b4adffce9a71457de308cdd83f485fee
DK\IRO <iro@sitecore.net>
2018-03-21 18:41:13 +0200

    Initial to fix Droptree field

---
+++ src/Sitecore.Support.211477/sitecore/Web/UI/HtmlControls/DataTreeview.cs
@@ -0,0 +1,66 @@
+﻿namespace Sitecore.Support.Web.UI.HtmlControls
+{
+  using Sitecore;
+  using Sitecore.Data.Items;
+  using Sitecore.Diagnostics;
+  using Sitecore.Web.UI.HtmlControls;
+
+  /// <summary>Displays hierarchical data, such as a table of contents, in a tree structure.</summary>
+  public class DataTreeview : Sitecore.Web.UI.HtmlControls.DataTreeview
+  {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="T:Sitecore.Web.UI.HtmlControls.DataTreeview" /> class.
+    /// </summary>
+    public DataTreeview() : base() { }
+
+    /// <summary>Gets the tree node.</summary>
+    /// <param name="item">The item.</param>
+    /// <param name="parent">The parent.</param>
+    /// <returns>The tree node.</returns>
+    protected override TreeNode GetTreeNode(Item item, System.Web.UI.Control parent)
+    {
+      Assert.ArgumentNotNull(item, "item");
+      Assert.ArgumentNotNull(parent, "parent");
+      DataTreeNode dataTreeNode = new DataTreeNode();
+      parent.Controls.Add(dataTreeNode);
+      dataTreeNode.Expandable = item.HasChildren;
+      dataTreeNode.Expanded = false;
+      dataTreeNode.Header = item.DisplayName;
+      dataTreeNode.Icon = item.Appearance.Icon;
+      dataTreeNode.ID = Sitecore.Web.UI.HtmlControls.Control.GetUniqueID("T");
+      dataTreeNode.ItemID = item.Paths.LongID;
+      dataTreeNode.ToolTip = item.Name;
+      dataTreeNode.DataContext = this.DataContext;
+      if (item.TemplateID != TemplateIDs.TemplateField)
+      {
+        dataTreeNode.ItemStyle = item.Appearance.Style;
+      }
+      if (dataTreeNode.ItemStyle.Length == 0)
+      {
+        if (item.Appearance.Hidden)
+        {
+          dataTreeNode.ItemStyle = "color:#666666";
+        }
+        else if (item.RuntimeSettings.IsVirtual)
+        {
+          dataTreeNode.ItemStyle = "color:#666666";
+        }
+      }
+      dataTreeNode.ShowCheckBox = base.ShowCheckboxes;
+      if (this.AllowDragging)
+      {
+        dataTreeNode.Drag = "item:" + item.Uri;
+        dataTreeNode.Drop = this.ID + ".DropItem(\"$Data\")";
+      }
+      foreach (string key in base.ColumnNames.Keys)
+      {
+        string text2 = ((BaseItem)item)[key];
+        if (text2.Length > 0)
+        {
+          dataTreeNode.ColumnValues[key] = text2;
+        }
+      }
+      return dataTreeNode;
+    }
+  }
+}
---
+++ src/Sitecore.Support.211477/sitecore/Web/UI/HtmlControls/TreePicker.cs
@@ -0,0 +1,63 @@
+﻿namespace Sitecore.Support.Web.UI.HtmlControls
+{
+  using Sitecore;
+  using Sitecore.Data.Items;
+  using Sitecore.Diagnostics;
+  using Sitecore.Globalization;
+  using Sitecore.Web.UI.HtmlControls;
+  using Sitecore.Web.UI.Sheer;
+  using System;
+  using System.Web.UI;
+  using System.Web.UI.WebControls;
+
+  /// <summary>
+  /// Represents a combobox with treeview dropdown.
+  /// </summary>
+  public class TreePicker : Sitecore.Web.UI.HtmlControls.TreePicker
+  {
+    /// <summary>
+    /// Called when the dropdown glyph is clicked.
+    /// </summary>
+    protected override void DropDown()
+    {
+      if (!string.IsNullOrEmpty(this.Value))
+      {
+        DataContext dataContext = Sitecore.Context.ClientPage.FindSubControl(this.DataContext) as DataContext;
+        Assert.IsNotNull(dataContext, typeof(DataContext), "Datacontext \"{0}\" not found.", this.DataContext);
+        dataContext.Folder = this.Value;
+      }
+      System.Web.UI.Control hiddenHolder = UIUtil.GetHiddenHolder(this);
+      DataTreeNode dataTreeNode = null;
+      Scrollbox scrollbox = new Scrollbox();
+      Sitecore.Context.ClientPage.AddControl(hiddenHolder, scrollbox);
+      scrollbox.Width = 300;
+      scrollbox.Height = 400;
+      DataTreeview dataTreeview = new DataTreeview();
+      dataTreeview.Class = "scTreeview scPopupTree";
+      dataTreeview.DataContext = this.DataContext;
+      dataTreeview.ID = this.ID + "_treeview";
+      dataTreeview.AllowDragging = false;
+      if (this.AllowNone)
+      {
+        dataTreeNode = new DataTreeNode();
+        Sitecore.Context.ClientPage.AddControl(dataTreeview, dataTreeNode);
+        dataTreeNode.ID = this.ID + "_none";
+        dataTreeNode.Header = Translate.Text("[none]");
+        dataTreeNode.Expandable = false;
+        dataTreeNode.Expanded = false;
+        dataTreeNode.Value = "none";
+        dataTreeNode.Icon = "Applications/16x16/forbidden.png";
+      }
+      Sitecore.Context.ClientPage.AddControl(scrollbox, dataTreeview);
+      dataTreeview.Width = new Unit(100.0, UnitType.Percentage);
+      dataTreeview.Click = this.ID + ".Select";
+      dataTreeview.DataContext = this.DataContext;
+      if (string.IsNullOrEmpty(this.Value) && dataTreeNode != null)
+      {
+        dataTreeview.ClearSelection();
+        dataTreeNode.Selected = true;
+      }
+      SheerResponse.ShowPopup(this.ID, "below-right", scrollbox);
+    }
+  }
+}
